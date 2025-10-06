507e87b260292e45bb05fac3ee41e1701f3a4901
unknown <your-email@example.com>
2025-10-06 19:49:56 +0530

    Fix: corrected import path case for Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Header from './Components/Header';
+import Header from './components/Header';
 import Footer from './Components/Footer';
 
 
